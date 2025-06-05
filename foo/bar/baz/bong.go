bd29fab051d9d8b78789cafa5b1865405cda5f10
AJ Christensen <achristensen@groq.com>
2025-06-06 10:17:37 +1200

    fix: test var

--- foo/bar/baz/bong.go
+++ foo/bar/baz/bong.go
@@ -2,3 +2,5 @@ package baz
 
 // Version is the current tagged release of the library.
 const Version = "0.0.1" // x-release-please-version
+
+var anotherVersion = "0.0.1" // x-release-please-version
